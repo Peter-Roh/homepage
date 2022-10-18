028d3b3de455e1816b8d822f1ddc657170c3d0c3
Peter-Roh <minchul.roh.peter@gmail.com>
2022-10-18 16:39:36 +0900

    tailwindcss 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <p>hello world</p>
+    <div className="App text-3xl font-bold prose prose-xl">
+      <p className="underline">hello world</p>
+      <button type="button" className="btn">
+        button
+      </button>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/**@type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: ["bumblebee"]
+  }
+}
